ed0812b3dc2d99d010e235f5c3b8a32c89cb1f25
kennyjung0223 <kenrohs@gmail.com>
2022-11-14 21:56:42 -0800

    add boilerplate

---
+++ main.py
@@ -0,0 +1,34 @@
+def add_two_nums(num1, num2):
+	return
+
+def subtract_two_nums(num1, num2):
+	return
+	
+def multiply_two_nums(num1, num2):
+	return
+
+def divide_two_nums(num1, num2):
+	# assume num2 is not equal to 0
+	return
+
+def modular_two_nums(num1, num2):
+	# assume num2 is not equal to 0
+	return
+
+def square_two_nums(num1):
+	# assume num1 > 0
+	return
+	
+def cube_two_nums(num1):
+	# assume num1 > 0
+	return
+
+
+if __name__ == '__main__':
+	add_two_nums(6, 3)				# expected: 9
+	subtract_two_nums(6, 3)				# expected: 3
+	multiply_two_nums(6, 3)				# expected: 18
+	divide_two_nums(6, 3)				# expected: 2
+	modular_two_nums(6, 3)				# expected: 0
+	square_two_nums(6)				# expected: 36
+	cube_two_nums(6)				# expected: 216
